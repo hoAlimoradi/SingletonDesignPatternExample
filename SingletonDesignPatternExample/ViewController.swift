5867f2194f6540e98b71309ded255317289d18c0
ho <ho.alimoradi@gmail.com>
2023-07-18 00:14:33 +0330

    Initial Commit

---
+++ SingletonDesignPatternExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingletonDesignPatternExample
+//
+//  Created by hoseinAlimoradi on 7/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
